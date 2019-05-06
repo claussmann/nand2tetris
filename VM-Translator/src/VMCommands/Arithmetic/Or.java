cdecf69340fad58b706020086d72c8db326e81cd
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-06 17:58:08 +0200

    ADD: 'Or'-Instruction

---
+++ VM-Translator/src/VMCommands/Arithmetic/Or.java
@@ -0,0 +1,21 @@
+package VMCommands.Arithmetic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Or extends ArithmeticCommand {
+    @Override
+    public List<String> toASMCommands() {
+        List<String> asm = new ArrayList<>();
+        asm.add("@SP");
+        asm.add("M=M-1");
+        asm.add("A=M");
+        asm.add("D=M");
+
+        asm.add("@SP");
+        asm.add("A=M-1");
+        asm.add("M=M|D");
+
+        return asm;
+    }
+}
