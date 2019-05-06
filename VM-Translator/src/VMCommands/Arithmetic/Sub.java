6b6d926def572f038ace28e5a8135db6d04e409c
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-06 17:31:30 +0200

    ADD: 'Sub'-Instruction

---
+++ VM-Translator/src/VMCommands/Arithmetic/Sub.java
@@ -0,0 +1,20 @@
+package VMCommands.Arithmetic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sub extends ArithmeticCommand {
+    @Override
+    public List<String> toASMCommands() {
+        List<String> asm = new ArrayList<>();
+        asm.add("@SP");
+        asm.add("M=M-1");
+        asm.add("A=M");
+        asm.add("D=M");
+
+        asm.add("@SP");
+        asm.add("A=M-1");
+        asm.add("M=D-M");
+        return asm;
+    }
+}
