924884bbdfa098872f262ebd8196c555394bf737
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-07 18:21:59 +0200

    REFACTOR: simplify 'not'

--- VM-Translator/src/VMCommands/ArithmeticCommands/Not.java
+++ VM-Translator/src/VMCommands/ArithmeticCommands/Not.java
@@ -4,31 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Not extends ArithmeticCommand {
-    static int x;
     @Override
     public List<String> toASMCommands() {
         List<String> asm = new ArrayList<>();
 
         asm.add("@SP");
         asm.add("A=M-1");
-
-        asm.add("@not"+x);
-        asm.add("D=M;JEQ");
-        // sp-1 == -1 ==> set sp-1 to false
-        asm.add("@SP");
-        asm.add("A=M-1");
-        asm.add("M=0");
-        asm.add("@continueNot"+x);
-        asm.add("0;JMP");
-
-        //sp-1 = false
-        asm.add("(not"+x+")");
-        asm.add("@SP");
-        asm.add("A=M-1");
-        asm.add("M=-1");
-
-        asm.add("(continueNot"+x+")");
-        x++;
+        asm.add("M=!M");
         return asm;
     }
 }
