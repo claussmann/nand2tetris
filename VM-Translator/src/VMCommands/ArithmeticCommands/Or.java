d70eb1aad461bf821b3b1ced9e3e695ba01d3648
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-07 18:27:49 +0200

    FIX: 'Or'

--- VM-Translator/src/VMCommands/ArithmeticCommands/Or.java
+++ VM-Translator/src/VMCommands/ArithmeticCommands/Or.java
@@ -8,9 +8,8 @@ public class Or extends ArithmeticCommand {
     public List<String> toASMCommands() {
         List<String> asm = new ArrayList<>();
         asm.add("@SP");
-        asm.add("M=M-1");
-        asm.add("A=M");
-        asm.add("D=M");
+        asm.add("AM=M-1");
+        asm.add("D=M"); //D = operand a
 
         asm.add("@SP");
         asm.add("A=M-1");
