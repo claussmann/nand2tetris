f518e556339bbb893e3ace4f9bf622a6d6d6b2d3
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-13 17:56:16 +0200

    ADD: FUNCTION-implementation

--- VM-Translator/src/VMCommands/FunctionCommands/Function.java
+++ VM-Translator/src/VMCommands/FunctionCommands/Function.java
@@ -4,12 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Function extends FunctionCommand {
+    private final String name;
+    private final int varc;
+
+    public Function(String name, int varc){
+        this.name = name;
+        this.varc = varc;
+    }
+
     @Override
     public List<String> toASMCommands() {
         List<String> asm = new ArrayList<>();
-        //save return address on stack
-        asm.add("");
-        asm.add("");
+
+        asm.add("("+name+")");
+
+        for (int i = 0; i < varc; i++) {
+            asm.add("@SP");
+            asm.add("A=M");
+            asm.add("M=0");
+            asm.add("@SP");
+            asm.add("M=M+1");
+        }
         return asm;
     }
 }
