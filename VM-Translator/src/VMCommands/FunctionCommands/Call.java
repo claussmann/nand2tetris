b1af3850e96fc6cc3414dab80edaf7a711877832
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-13 17:11:50 +0200

    ADD: CALL-Implementation

--- VM-Translator/src/VMCommands/FunctionCommands/Call.java
+++ VM-Translator/src/VMCommands/FunctionCommands/Call.java
@@ -1,10 +1,70 @@
 package VMCommands.FunctionCommands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Call extends FunctionCommand {
+    static int i;
+    private final String function;
+    private final int argc;
+
+    public Call(String function, int argc){
+        this.function = function;
+        this.argc = argc;
+    }
     @Override
     public List<String> toASMCommands() {
-        return null;
+        List<String> asm = new ArrayList<>();
+        //save return address on stack
+        asm.add("@ret."+i);
+        asm.add("D=A");
+        asm.addAll(pushD());
+
+        //save own variables
+        asm.add("@LCL");
+        asm.add("D=M");
+        asm.addAll(pushD());
+
+        asm.add("@ARG");
+        asm.add("D=M");
+        asm.addAll(pushD());
+
+        asm.add("@THIS");
+        asm.add("D=M");
+        asm.addAll(pushD());
+
+        asm.add("@THAT");
+        asm.add("D=M");
+        asm.addAll(pushD());
+
+        //put the arg-pointer to the correct position
+        int stackOffset = (5+argc);
+        asm.add("@"+stackOffset);
+        asm.add("D=A");
+        asm.add("@SP");
+        asm.add("D=M-D");
+        asm.add("@ARG");
+        asm.add("M=D");
+
+        //jump to function
+        asm.add("@"+function);
+        asm.add("=;JMP");
+
+        //return here:
+        asm.add("(ret."+i+")");
+
+        return asm;
+    }
+
+    private List<String> pushD() {
+        List<String> asm = new ArrayList<>();
+
+        asm.add("@SP");
+        asm.add("A=M");
+        asm.add("M=D");
+        asm.add("@SP");
+        asm.add("M=M+1");
+
+        return asm;
     }
 }
