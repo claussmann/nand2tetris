95ce6f926ed149d80436837462fedd06ffda79f1
Christian Laussmann <ch.laussmann@gmail.com>
2019-06-04 21:39:42 +0200

    ADD: Statements is a list-wrapper

--- JackCompiler/src/Parser/Statements.java
+++ JackCompiler/src/Parser/Statements.java
@@ -1,4 +1,14 @@
 package Parser;
 
+import java.util.ArrayList;
+
 public class Statements {
+    ArrayList list;
+    public Statements (){
+        list=new ArrayList();
+    }
+
+    public void add(Statement statement){
+        list.add(statement);
+    }
 }
