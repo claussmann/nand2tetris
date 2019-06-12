2bf730f1b7df835a58a2e2ef37532042a4d69716
Christian Laussmann <ch.laussmann@gmail.com>
2019-06-12 13:01:24 +0200

    REFACTOR: Handle Errors more easily

--- JackCompiler/src/JackCompiler.java
+++ JackCompiler/src/JackCompiler.java
@@ -26,45 +26,28 @@ public class JackCompiler {
         Tokenizer tokenizer=new Tokenizer();
         Queue<Token> tokens = tokenizer.tokenize(allLines);
 
-
         PrintStream printStream = new PrintStream(new FileOutputStream(args[0] + "T.xml"));
+        System.out.println("Printing Tokens to "+args[0]+"T.xml");
         for (Token token:tokens){
             printStream.println(token.toXML());
         }
 
         Program program = new Program(tokens);
-
         try{
             program.parse();
         }catch (Exception e){
             e.printStackTrace();
-            System.err.println();
-            System.err.println();
-            System.err.println("Failed to parse tokenized file in region:");
-            System.err.println();
-            System.err.println("(...)");
-            System.err.println(tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken()+" "+
-                                tokens.remove().getToken());
-            System.err.println("(...)");
-
+            System.err.println("Failed to parse in region:");
+            for(int i=0; i<10;i++){
+                System.out.print(tokens.remove().toXML());
+            }
             System.exit(-1);
         }
 
-        PrintStream printStream2 = new PrintStream(new FileOutputStream(args[0] + ".xml"));
-        program.toXML(printStream2);
+        if(program.isValid()) {
+            System.out.println("Writing to "+args[0]+".xml");
+            PrintStream printStream2 = new PrintStream(new FileOutputStream(args[0] + ".xml"));
+            program.toXML(printStream2);
+        }
     }
 }
